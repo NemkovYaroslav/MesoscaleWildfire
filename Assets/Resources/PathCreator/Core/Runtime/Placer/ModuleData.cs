4928b9a147b75e11535c17ec2ebfb26ed77ceaf1
Nemkov Yaroslav <nemkofff.slava@gmail.com>
2024-04-19 20:34:53 +0300

    [Branch] Change Module Placement Logic

---
+++ Assets/Resources/PathCreator/Core/Runtime/Placer/ModuleData.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace Resources.PathCreator.Core.Runtime.Placer
+{
+    public class Module : MonoBehaviour
+    {
+        [Range(0.0f, 1.0f)] public float radius = 1.0f;
+    }
+}
