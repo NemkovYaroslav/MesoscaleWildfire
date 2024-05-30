619f634f0864b5aa3063714c3fed2ff5bb04292b
Nemkov Yaroslav <nemkofff.slava@gmail.com>
2024-05-30 07:03:43 +0300

    [Common] Add Image

---
+++ Assets/Scripts/WildfireModel/Wildfire/ForestFiller.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace WildfireModel.Wildfire
+{
+    public class ForestFiller : MonoBehaviour
+    {
+        [Header("Spawn Settings")]
+        public GameObject treePrefab;
+        public float distanceBetweenTrees;
+        public Vector3 positivePosition;
+        public Vector3 negativePosition;
+        
+        private void Awake()
+        {
+            SpawnResources();
+        }
+
+        private void SpawnResources()
+        {
+            for (var x = negativePosition.x; x < positivePosition.x; x += distanceBetweenTrees)
+            {
+                for (var z = negativePosition.z; z < positivePosition.z; z += distanceBetweenTrees)
+                {
+                    Instantiate(treePrefab, new Vector3(x, -6.5f, z), Quaternion.Euler(new Vector3(0, Random.Range(0, 360), 0)));
+                }
+            }
+        }
+    }
+}
